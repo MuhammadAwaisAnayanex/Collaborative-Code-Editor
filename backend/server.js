02c1b8ad101f7bb13455292e57fffab59f51c185
Muhammad Awais <awaisilyas800@gmail.com>
2025-02-27 21:44:19 +0500

    create user model routes file and connection to mongoDB DataBase

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const mongoose = require('mongoose');
 const cors = require('cors');
+const projectRoutes = require('./routes/projectRoutes');
 require('dotenv').config();
 
 const app = express();
@@ -17,7 +19,10 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.json());
 
-
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Socket.io Connection
 io.on('connection', (socket) => {
@@ -34,5 +39,8 @@ io.on('connection', (socket) => {
 });
 
 // Start Server
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
+
+app.use('/api/projects', projectRoutes);
+
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
